bbc4687ce10bf4230d8fa881533b91bfe5ee1b94
Valeria Sandoval <valeriamss@gmail.com>
2020-06-03 19:01:11 -0400

    ayuda se agrego comentario

--- doc/help/help.php
+++ doc/help/help.php
@@ -10,5 +10,6 @@ include_once APP_ROOT."/app/core/utils.php";
 echo f_html_header('Help');
 echo f_app_mainmenu();
 echo '<h1>How can I help You!<h1>';
+echo '<h1 style="color:blue;" >Aqui mi cambio (Vale) <h1>';
 echo f_html_footer();
 ?>
